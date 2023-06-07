7b402c61bc8fb7e2d710246b123f29d22c712a41
Andrei <andrei.m2005@aluno.ifsc.edu.br>
2023-06-07 10:48:53 -0300

    Complemento MVC
    Andrei e Gabriel

---
+++ Estoque_Livraria/src/Modelo/Livro.java
@@ -0,0 +1,42 @@
+package Modelo;
+
+public class Livro {
+
+	private String titulo;
+	private String autor;
+	private String genero;
+	private Double preco;
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+}
---
+++ Estoque_Livraria/src/Visao/Main.java
@@ -0,0 +1,75 @@
+package Visao;
+
+import java.util.Scanner;
+
+import Controle.LivroDAO;
+import Modelo.Livro;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Integer opcaoSelecionada = Integer.MAX_VALUE;
+		Scanner leitura = new Scanner(System.in);
+		     
+		       
+		LivroDAO estoque = new LivroDAO();
+
+		while (opcaoSelecionada != 0) {
+
+			System.out.println("- MENU EXEMPLO -");
+			System.out.println("0 SAIR");
+			System.out.println("1 CADASTRAR LIVRO");
+			System.out.println("2 BUSCAR LIVRO");
+			System.out.println("3 EFETUAR COMPRAR");
+			System.out.println("4 Listar");
+	
+			opcaoSelecionada = Integer.valueOf(leitura.nextLine());
+
+			switch (opcaoSelecionada) {
+			case 0: {
+				System.out.println("Encerrado");
+				break;
+			}
+			case 1: {
+				Livro a=new Livro();
+				System.out.println("Informe o título: ");
+				a.setTitulo(leitura.nextLine());
+				System.out.println("Informe o autor: ");
+				a.setAutor(leitura.nextLine());
+				System.out.println("Informe o gênero literário: ");
+				a.setGenero(leitura.nextLine());
+				System.out.println("Informe o preço: ");
+				a.setPreco(Double.valueOf(leitura.nextLine()));	
+				
+				estoque.Inserir(a);
+				
+				
+			}
+			case 2: {
+			
+				
+			
+				
+			}
+			case 3: {
+				/*
+				 * Realiza a função de compra atraves do arraylist
+				 * Caso a compra seja efetuada retira o livro do estoque
+				 * 
+				 */
+				
+			
+			}
+			case 4:{
+				estoque.listar();
+			}
+
+			}
+
+		}
+
+		leitura.close();
+	}
+
+}
